acf078a513c78cfe2f585af44f7f671537b81ae0
mohamed-misarh2 <115933698+mohamed-misarh2@users.noreply.github.com>
2024-09-20 16:00:09 +0300

    Add files via upload

---
+++ E-Learning.Application/Mapper/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Learning.Application.Mapper
+{
+    public class AutoMapperProfile:Profile
+    {
+        public AutoMapperProfile()
+        {
+        }
+    }
+}
