3d4a1ef75efd3ede379ae0a48d0cdc3c089daafa
mohamed-misarh2 <115933698+mohamed-misarh2@users.noreply.github.com>
2024-09-20 16:00:46 +0300

    Add files via upload

---
+++ E-Learning.Context/ELearningContext.cs
@@ -0,0 +1,30 @@
+﻿using E_Learning.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Learning.Context
+{
+    public class ELearningContext: IdentityDbContext<User>
+    {
+
+        public DbSet<Assignment> Assignments { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Lecture> Lectures { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderCourse> OrderCourses { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+
+        public ELearningContext(DbContextOptions<ELearningContext> options) :base(options)
+        { }
+    }
+}
