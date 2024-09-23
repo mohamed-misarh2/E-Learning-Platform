186b83a11b3d7faf05fe47730a12ea2ddbb9c267
AyaAhmed18 <yoyoahmed108@gmail.com>
2024-09-22 22:17:26 -0700

    edit1

--- E-Learning.Dtos/obj/Debug/net8.0/E-Learning.Dtos.AssemblyInfo.cs
+++ E-Learning.Dtos/obj/Debug/net8.0/E-Learning.Dtos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Learning.Dtos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da1fe4d2851d1791fddd7501838dc2fa2c49ba11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cba44916f2bcf64ff89f9254e69a96b770993e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Learning.Dtos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Learning.Dtos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
