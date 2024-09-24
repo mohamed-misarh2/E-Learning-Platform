e7ff4ef4bd0495991ed36b936b4097f6412234a0
AyaAhmed18 <yoyoahmed108@gmail.com>
2024-09-23 21:58:44 -0700

    Updated .gitignore to exclude unnecessary files

--- E-Learning.Application/Services/TopicService.cs
+++ E-Learning.Application/Services/TopicService.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
 using E_Learning.Application.Contract;
 using E_Learning.Application.IService;
+using E_Learning.Dtos.Category;
 using E_Learning.Dtos.Topic;
 using E_Learning.Dtos.ViewResult;
 using E_Learning.Models;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,34 +23,154 @@ namespace E_Learning.Application.Services
             _mapper = mapper;
             _topicRepository = topicRepository;
         }
-        public Task<ResultView<CreateOrUpdateTopicDTO>> CreateTopicAsync(Topic topic)
+        public async Task<ResultView<CreateOrUpdateTopicDTO>> CreateTopicAsync(CreateOrUpdateTopicDTO topic)
         {
-            throw new NotImplementedException();
+            ResultView<CreateOrUpdateTopicDTO> result = new();
+            try { 
+            var topics = await _topicRepository.GetAllAsync();
+            var oldTopic = topics.Where(i => i.Name == topic.Name).FirstOrDefault();
+           
+            if (oldTopic != null)
+            {
+                result.IsSuccess = false;
+                result.Entity = null;
+                result.Message = "Topc is already found";
+                return result;
+            }
+            var Topic= _mapper.Map<Topic>(topic);
+            var NewTopic = await _topicRepository.CreateAsync(Topic);
+             await _topicRepository.SaveChangesAsync();
+            var NewTopicDto = _mapper.Map<CreateOrUpdateTopicDTO>(NewTopic);
+            result.Entity=NewTopicDto;
+            result.IsSuccess=true;
+            result.Message = "Topic Created Successfully";
+            return result;
+            }
+            catch (Exception ex)
+            {
+                result.IsSuccess = false;
+                result.Entity = null;
+                result.Message = "Something went wrong";
+                return result;
+
+            }
         }
 
-        public Task<ResultView<List<GetAllTopicDTO>>> GetAllTopicsAsync()
+        public async Task<ResultDataList<GetAllTopicDTO>> GetAllTopicsAsync()
         {
-            throw new NotImplementedException();
+            try { } catch (Exception ex) { }
+            var Topics = await _topicRepository.GetAllAsync();
+
+            var TopicDTO = Topics.Where(c => c.IsDeleted == false).Select(c => new GetAllTopicDTO
+            {
+                Id = c.Id,
+                Name = c.Name,
+                NumberOfCourses = c.Courses.Count(),
+                NumberOfStudents = c.Courses.Select(i => i.UserId).Sum()
+            }).ToList();
+
+            ResultDataList<GetAllTopicDTO> result = new ResultDataList<GetAllTopicDTO>();
+            result.Entities = TopicDTO;
+            result.Count = TopicDTO.Count();
+            return result;
         }
 
-        public Task<ResultView<GetAllTopicDTO>> GetTopicAsync(Guid topicId)
+        public async Task<ResultView<GetAllTopicDTO>> GetTopicAsync(Guid topicId)
         {
-            throw new NotImplementedException();
+            var Topic = await _topicRepository.GetByIdAsync(topicId);
+            if(Topic == null)
+            {
+                return new ResultView<GetAllTopicDTO> { Entity = null, IsSuccess = false,Message="Topic is not found" };
+
+            }
+            var TopicDto = _mapper.Map<GetAllTopicDTO>(Topic);
+            return new ResultView<GetAllTopicDTO> { Entity = TopicDto, IsSuccess = true, Message="Topic found Successfully" };
         }
 
-        public Task<ResultView<CreateOrUpdateTopicDTO>> HardDeleteTopicAsync(Guid topicId)
+        public async Task<ResultView<CreateOrUpdateTopicDTO>> HardDeleteTopicAsync(Guid topicId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var Topic = await _topicRepository.GetByIdAsync(topicId);
+                if (Topic == null)
+                {
+                    return new ResultView<CreateOrUpdateTopicDTO> { Entity = null, IsSuccess = true, Message = "Topic is not Found" };
+
+                }
+                else
+                {
+                    var oldTopic = _topicRepository.DeleteAsync(Topic);
+                    await _topicRepository.SaveChangesAsync();
+                    var TopicDto = _mapper.Map<CreateOrUpdateTopicDTO>(oldTopic);
+                    return new ResultView<CreateOrUpdateTopicDTO> { Entity = TopicDto, IsSuccess = true, Message = "Deleted Successfully" };
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ResultView<CreateOrUpdateTopicDTO> { Entity = null, IsSuccess = false, Message = ex.Message };
+
+            }
         }
 
-        public Task<ResultView<CreateOrUpdateTopicDTO>> SoftDeleteTopicAsync(Guid topicId)
+        public async Task<ResultView<CreateOrUpdateTopicDTO>> SoftDeleteTopicAsync(Guid topicId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var Topic = await _topicRepository.GetByIdAsync(topicId);
+                if (Topic == null) 
+                {
+                    return new ResultView<CreateOrUpdateTopicDTO> { Entity = null, IsSuccess = true, Message = "This Topic is not found" };
+
+                }
+                else 
+                {
+                    Topic.IsDeleted = true;
+                    await _topicRepository.SaveChangesAsync();
+
+                    var TopicDto = _mapper.Map<CreateOrUpdateTopicDTO>(Topic);
+                    return new ResultView<CreateOrUpdateTopicDTO> { Entity = TopicDto, IsSuccess = true, Message = "Deleted Successfully" };
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ResultView<CreateOrUpdateTopicDTO> { Entity = null, IsSuccess = false, Message = ex.Message };
+
+            }
         }
 
-        public Task<ResultView<CreateOrUpdateTopicDTO>> UpdateTopicAsync(Topic topic)
+        public async Task<ResultView<CreateOrUpdateTopicDTO>> UpdateTopicAsync(CreateOrUpdateTopicDTO topic)
         {
-            throw new NotImplementedException();
+            ResultView<CreateOrUpdateTopicDTO> result = new();
+            try
+            {
+                var oldTopic = await _topicRepository.GetByIdAsync(topic.Id);
+              
+                if (oldTopic == null)
+                {
+                    result.IsSuccess = false;
+                    result.Entity = null;
+                    result.Message = "Topc is not found";
+                    return result;
+                }
+                var Ntopic = _mapper.Map<Topic>(topic);
+                var Newtopic = await _topicRepository.UpdateAsync(Ntopic);
+                await _topicRepository.SaveChangesAsync();
+                var NewTopicDto = _mapper.Map<CreateOrUpdateTopicDTO>(Newtopic);
+                result.Entity = NewTopicDto;
+                result.IsSuccess = true;
+                result.Message = "Topic Updated Successfully";
+                return result;
+            }
+            catch (Exception ex)
+            {
+                result.IsSuccess = false;
+                result.Entity = null;
+                result.Message = "Something went wrong";
+                return result;
+
+            }
         }
     }
 }
