758c4ae0a0cd84615dc968109dba4af5b0f8655c
AyaAhmed18 <yoyoahmed108@gmail.com>
2024-10-04 12:53:35 +0300

    m

--- E-Learning.Dtos/Topic/CreateOrUpdateTopicDTO.cs
+++ E-Learning.Dtos/Topic/CreateOrUpdateTopicDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,9 @@ namespace E_Learning.Dtos.Topic
     public class CreateOrUpdateTopicDTO
     {
         public Guid Id { get; set; }
-        
+        [Required]
         public string Name { get; set; }
+        [Required]
         public Guid SubCategoryId {  get; set; }
     }
 }
